95fe672168d768bee0fbc6f8f94e272b6b858eaa
Stephen Bolton <stephen@bitsonthemind.com>
2021-02-09 23:48:27 -0700

    Fix silent option in lsp mappings

--- nvim/nvim/lua/mappings/lsp.lua
+++ nvim/nvim/lua/mappings/lsp.lua
@@ -1,12 +1,12 @@
 local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true}
 
-map('n', '<silent>', '<leader>ld <cmd>lua vim.lsp.buf.definition()<CR>', default_opts)
-map('n', '<silent> <leader>lD', ':vsp <BAR> :lua vim.lsp.buf.definition()<CR>zz', default_opts)
-map('n', '<silent> <leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<CR>', default_opts)
-map('n', '<silent> <leader>lh', '<cmd>lua vim.lsp.buf.hover()<CR>', default_opts)
-map('n', '<silent> <leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', default_opts)
-map('n', '<leader>lo', ':packadd vista.vim <BAR> Vista finder skim<CR>', default_opts)
+map('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true, silent = true})
+map('n', '<leader>lD', ':vsp <BAR> :lua vim.lsp.buf.definition()<CR>zz', { noremap = true, silent = true })
+map('n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
+map('n', '<leader>lh', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+map('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting_sync(nil, 1000)<CR>', default_opts)
+map('n', '<leader>lo', '<cmd>lua require("telescope.builtin").lsp_document_symbols()<CR>', default_opts)
 map('n', '<leader>lO', ':packadd vista.vim <BAR> Vista<CR>', default_opts)
 map('n', '<leader>lp', ':CocList diagnostics<CR>', default_opts)
 map('n', '<leader>l?', ':CocInfo<CR>', default_opts) 

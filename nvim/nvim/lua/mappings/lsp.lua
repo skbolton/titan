2fa09c1ca0463f04f3c9ad222f3511b4d7981818
Stephen Bolton <stephen@bitsonthemind.com>
2021-02-11 21:30:47 -0700

    Fix vista lazy loading

--- nvim/nvim/lua/mappings/lsp.lua
+++ nvim/nvim/lua/mappings/lsp.lua
@@ -7,7 +7,7 @@ map('n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap = 
 map('n', '<leader>lh', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
 map('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting_sync(nil, 1000)<CR>', default_opts)
 map('n', '<leader>lo', '<cmd>lua require("telescope.builtin").lsp_document_symbols()<CR>', default_opts)
-map('n', '<leader>lO', ':packadd vista.vim <BAR> Vista<CR>', default_opts)
+map('n', '<leader>lO', ':Vista<CR>', {noremap = true, silent = true})
 map('n', '<leader>lp', ':CocList diagnostics<CR>', default_opts)
 map('n', '<leader>l?', ':CocInfo<CR>', default_opts) 
 map('n', '<leader>lr', '<cmd>lua vim.lsp.stop_client(vim.lsp.get_active_clients())<CR> :edit<CR>', default_opts) 

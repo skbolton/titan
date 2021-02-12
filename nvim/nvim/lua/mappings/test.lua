80600e85c1e329a427f627f9b3120ca1671dfc7a
Stephen Bolton <stephen@bitsonthemind.com>
2021-02-11 21:20:15 -0700

    Fix vimtest command

--- nvim/nvim/lua/mappings/test.lua
+++ nvim/nvim/lua/mappings/test.lua
@@ -3,7 +3,7 @@ local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
 
 map('n', '<leader>tt', ':TestFile<CR>', default_opts)
-map('n', '<leader>tT', ':TestFile --strategy=neovim<CR>', default_opts)
+map('n', '<leader>tT', ':TestFile -strategy=neovim<CR>', default_opts)
 map('n', '<leader>tn', ':TestNearest<CR>', default_opts)
 map('n', '<leader>tN', ':TestNearest -strategy=neovim<CR>', default_opts)
 map('n', '<leader>t.', ':TestLast<CR>', default_opts)

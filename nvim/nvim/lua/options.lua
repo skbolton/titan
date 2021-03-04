a77b5ac8181fd44f9d49b5b6cefadb8e5698225c
Stephen Bolton <stephen@bitsonthemind.com>
2021-03-03 18:30:22 -0700

    Fix setting editor options for buffer/window options

--- nvim/nvim/lua/options.lua
+++ nvim/nvim/lua/options.lua
@@ -12,72 +12,93 @@
                                                             
 --]]
 
+-- Set editor options in lua
 -- instead of `set option=...`
-local o = vim.o
--- for options that are local to a window
-local wo = vim.wo
+-- Unlike vim, for buffer or window options we also need to set
+-- the global table or else only the first buffer/window will
+-- have the option
+--
+-- To see if an option is global, buffer, or window scoped you
+-- have to run :help <option> and see before its description 
+-- what option it is.
+--
+-- Here is an excert of `:help expandtab`
+--  'expandtab' 'et'	boolean	(default off)
+--    local to buffer <- HERE
+--  In Insert mode: Use the appropriate number of spaces to insert a
+--
+-- Usage:
+-- opt('g', 'termguicolors', true)
+-- opt('b', 'softtabstop', 2)
+-- opt('w', 'wrap', true)
+local function opt(scope, key, value)
+  local scopes = {g = vim.o, b = vim.bo, w = vim.wo}
+
+  scopes[scope][key] = value
+  if scope ~= 'g' then scopes['g'][key] = value end
+end
 
 -- SPACING AND INDENTATION
 -- ===================================================================
-o.tabstop = 2
-o.softtabstop = 2
-o.shiftwidth = 2
-o.expandtab = true
-o.smartindent = true
-o.autoindent = true
-o.wrap = true
-o.linebreak = true
+opt('b', 'tabstop', 2)
+opt('b', 'softtabstop', 2)
+opt('b', 'shiftwidth', 2)
+opt('b', 'expandtab', true)
+opt('b', 'smartindent', true)
+opt('b', 'autoindent', true)
+opt('w', 'wrap', true)
+opt('w', 'linebreak', true)
 
 -- UI CONFIG
 -- ===================================================================
 -- gutters, line numbers, searching
-o.termguicolors = true
-o.encoding = "utf8"
-wo.number = true
-wo.relativenumber = true
-o.ruler = true
-o.cursorline = true
+opt('g', 'termguicolors', true)
+opt('g', 'encoding', "utf8")
+opt('w', 'number', true)
+opt('w', 'relativenumber', true)
+opt('g', 'ruler', true)
+opt('g', 'cursorline', true)
 -- when typing a bracket highlight the match
-o.showmatch = true
+opt('g', 'showmatch', true)
 -- no idea what this does?
-o.list = true
+opt('g', 'list', true)
 -- list chars for 'hidden' characters
-wo.lcs = "extends:❯,precedes:❮,tab:>-"
+opt('w', 'lcs', "extends:❯,precedes:❮,tab:>-")
 -- alsways show symbol gutter (I hate seeing the editor shift)
-wo.signcolumn = 'yes'
+opt('w', 'signcolumn', 'yes')
 -- highlight words that match / search
-o.hlsearch = true
+opt('g', 'hlsearch', true)
 -- start hlsearch as I type
-o.incsearch = true
-o.ignorecase = true
+opt('g', 'incsearch', true)
+opt('g', 'ignorecase', true)
 -- search case insensitive until I add a capital letter
-o.smartcase = true
+opt('g', 'smartcase', true)
 -- show effects of substitution as I perform it
-o.inccommand = "nosplit"
+opt('g', 'inccommand', "nosplit")
 -- Don't show what mode I am in
 -- My statusline will tell me this not the editor
-o.showmode = false
+opt('g', 'showmode', false)
 -- how often to poll for updates
 -- affects things like gutter symbols
-o.updatetime = 300
-o.lazyredraw = true
+opt('g', 'updatetime', 300)
+opt('g', 'lazyredraw', true)
 -- fix the crazy default behavior of how splits are created
-o.splitbelow = true
-o.splitright = true
+opt('g', 'splitbelow', true)
+opt('g', 'splitright', true)
 -- MENUS
-o.wildmenu = true
-o.completeopt = "menuone,noselect,noinsert"
-o.history = 100
+opt('g', 'wildmenu', true)
+opt('g', 'completeopt', "menuone,noselect,noinsert")
+opt('g', 'history', 100)
 
 -- FOLDING
 -- ===================================================================
-o.foldenable = true
-o.foldlevelstart = 10
-o.foldnestmax = 10
+opt('g', 'foldenable', true)
+opt('g', 'foldlevelstart', 10)
+opt('g', 'foldnestmax', 10)
 
 -- BACKUP/SWAP
 -- ===================================================================
 -- I also like to live dangerously
-o.backup = false
-o.writebackup = false
-o.swapfile = false
+opt('g', 'backup', false)
+opt('g', 'writebackup', false)
+opt('g', 'swapfile', false)

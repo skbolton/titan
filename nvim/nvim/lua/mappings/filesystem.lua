c4a886988f28698646637a177a7f5e1e0cc4c2ba
Stephen Bolton <stephen@bitsonthemind.com>
2021-03-21 13:39:39 -0600

    Add some small filesystem mapping tweaks

--- nvim/nvim/lua/mappings/filesystem.lua
+++ nvim/nvim/lua/mappings/filesystem.lua
@@ -4,5 +4,6 @@ local default_opts = {noremap = true}
 map('n', '<leader>\\', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
 map('n', '<leader><bar>', ':NvimTreeFindFile<CR>', {noremap = true, silent = true})
 map('n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files()<cr>", default_opts)
-map('n', '<leader>fr', "<cmd>lua require'telescope.builtin'.buffers()<cr>", default_opts)
-map('n', '<leader>/', "<cmd>lua require'telescope.builtin'.live_grep()<cr>", default_opts)
+map('n', '<leader>fr', "<cmd>lua require'telescope.builtin'.buffers({ show_all_buffers = true })<cr>", default_opts)
+map('n', '<leader>/', ":silent grep ", default_opts)
+map('n', '<leader>_', "<cmd>lua require'telescope.builtin'.live_grep()<cr>", default_opts)

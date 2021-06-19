53ee4b9c3b17386fe8023c7ec9754526d53f7983
Stephen Bolton <stephen@bitsonthemind.com>
2021-06-19 13:04:37 -0600

    Add financials folder to rally

--- tmux/rally.sh
+++ tmux/rally.sh
@@ -5,7 +5,7 @@ set -eu
 RALLY=rally
 VERSION=0.0.1
 
-TARGET=$(ls -d ~/Repos/* ~/* /srv/* | sk)
+TARGET=$(ls -d ~/Repos/* ~/* /srv/* ~/Documents/Archive/Finances/* | sk)
 NAME=$(basename $TARGET)
 
 smug start $NAME -a || smug start launch -a name=$NAME root=$TARGET

ec753f03d3eb5928f6cd8b1c912f3ee360bbb851
Stephen Bolton <stephen@bitsonthemind.com>
2021-06-25 17:12:08 -0600

    Rename launch smug template to default

--- tmux/rally.sh
+++ tmux/rally.sh
@@ -8,4 +8,4 @@ VERSION=0.0.1
 TARGET=$(ls -d ~/Repos/* ~/* /srv/* ~/Documents/Archive/Finances/* | sk)
 NAME=$(basename $TARGET)
 
-smug start $NAME -a || smug start launch -a name=$NAME root=$TARGET
+smug start $NAME -a || smug start default -a name=$NAME root=$TARGET

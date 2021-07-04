18057ea905de609b9d151253792a3161a71a0fce
Stephen Bolton <stephen@bitsonthemind.com>
2021-07-03 20:18:24 -0600

    Add git branches command back to telescope

--- nvim/nvim/lua/mappings/filesystem.lua
+++ nvim/nvim/lua/mappings/filesystem.lua
@@ -6,6 +6,7 @@ map('n', '<leader><bar>', ':NvimTreeFindFile<CR>', {noremap = true, silent = tru
 map('n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", default_opts)
 map('n', '<leader>fr', "<cmd>lua require'telescope.builtin'.buffers({ show_all_buffers = true })<cr>", default_opts)
 map('n', '<leader>fg', "<cmd>lua require'telescope.builtin'.git_status()<cr>", default_opts)
+map('n', '<leader>fb', "<cmd>lua require'telescope.builtin'.git_branches()<cr>", default_opts)
 map('n', '<leader>f?', ":TodoTelescope<cr>", default_opts)
 map('n', '<leader>/', ":silent grep ", default_opts)
 map('n', '<leader>_', "<cmd>lua require'telescope.builtin'.live_grep()<cr>", default_opts)

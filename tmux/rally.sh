2fac631922e55cb18a6f6ce8d5eebae5c4fabfe3
Stephen Bolton <stephen@bitsonthemind.com>
2021-07-04 22:07:10 -0600

    Update rally to see Delta files and remove old finance files

--- tmux/rally.sh
+++ tmux/rally.sh
@@ -5,7 +5,7 @@ set -eu
 RALLY=rally
 VERSION=0.0.1
 
-TARGET=$(ls -d ~/Repos/* ~/* /srv/* ~/Documents/Archive/Finances/* | sk)
+TARGET=$(ls -d ~/Repos/* ~/* /srv/* ~/Documents/* | sk)
 NAME=$(basename $TARGET)
 
 smug start $NAME -a || smug start default -a name=$NAME root=$TARGET

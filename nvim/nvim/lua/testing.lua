85baaeeb8f9e55de164232edd45f10441d0b297c
Stephen Bolton <stephen@bitsonthemind.com>
2021-07-17 06:00:05 -0600

    Flip test strategies around so that tmux spilt is standard

--- nvim/nvim/lua/testing.lua
+++ nvim/nvim/lua/testing.lua
@@ -2,14 +2,15 @@ local g = vim.g
 local v = vim.v
 
 g.VimuxOrientation = "h"
+g.VimuxHeight = "30"
 
 g["test#preserve_screen"] = false
 g['test#javascript#jest#options'] = '--reporters jest-vim-reporter'
 g.neomake_open_list = true
 g['test#strategy'] = {
-  nearest = 'neomake',
-  file = 'neomake',
-  suite = 'neomake'
+  nearest = 'vimux',
+  file = 'vimux',
+  suite = 'vimux'
 }
 g['test#neovim#term_position'] = 'vert'
 g.neomake_warning_sign = {

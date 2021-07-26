e895c0944307da33e3646152ee66607cd15d211d
Stephen Bolton <stephen@bitsonthemind.com>
2021-07-25 18:54:05 -0600

    Improve hotkeys for awesome

--- awesome/awesome/keys.lua
+++ awesome/awesome/keys.lua
@@ -3,38 +3,36 @@ local gears = require("gears")
 local bling = require("module.bling")
 local hotkeys_popup = require("awful.hotkeys_popup")
 
-local superkey = "Mod4"
-local altkey = "Mod1"
+local super = "Mod4"
+local shift = "Shift"
+local ctrl = "Control"
+local alt = "Mod1"
 
 local globalkeys = gears.table.join(
-    awful.key({ superkey }, "Escape",
-      awful.tag.history.restore,
-      {description = "go back", group = "tag"}
-    ),
-    -- START MY KEYS
+    -- CLIENT KEYS
     -- move by directions
-    awful.key({ superkey }, "h",
+    awful.key({ super }, "h",
       function (c)
         awful.client.focus.bydirection("left", c)
         bling.module.flash_focus.flashfocus(client.focus)
       end,
       {description = "focus left", group = "client"}
     ),
-    awful.key({ superkey }, "j",
+    awful.key({ super }, "j",
       function (c)
           awful.client.focus.bydirection("down", c)
           bling.module.flash_focus.flashfocus(client.focus)
       end,
       {description = "focus down", group = "client"}
     ),
-    awful.key({ superkey }, "k",
+    awful.key({ super }, "k",
       function (c)
           awful.client.focus.bydirection("up", c)
           bling.module.flash_focus.flashfocus(client.focus)
       end,
       {description = "focus up", group = "client"}
     ),
-    awful.key({ superkey }, "l",
+    awful.key({ super }, "l",
       function (c)
           awful.client.focus.bydirection("right", c)
           bling.module.flash_focus.flashfocus(client.focus)
@@ -42,127 +40,167 @@ local globalkeys = gears.table.join(
       {description = "focus right", group = "client"}
     ),
     -- Swap by direction
-    awful.key({ superkey, "Control"   }, "h",
+    awful.key({ super, ctrl }, "h",
       function (c)
         awful.client.swap.bydirection("left", c)
         bling.module.flash_focus.flashfocus(client.focus)
       end,
       {description = "swap left", group = "client"}
     ),
-    awful.key({ superkey, "Control"   }, "j",
+    awful.key({ super, ctrl }, "j",
       function (c)
         awful.client.swap.bydirection("down", c)
         bling.module.flash_focus.flashfocus(client.focus)
       end,
       {description = "swap down", group = "client"}
     ),
-    awful.key({ superkey, "Control"   }, "k",
+    awful.key({ super, ctrl }, "k",
       function (c)
         awful.client.swap.bydirection("up", c)
         bling.module.flash_focus.flashfocus(client.focus)
       end,
       {description = "swap up", group = "client"}
     ),
-    awful.key({ superkey, "Control"   }, "l",
+    awful.key({ super, ctrl }, "l",
       function (c)
         awful.client.swap.bydirection("right", c)
         bling.module.flash_focus.flashfocus(client.focus)
       end,
       {description = "swap right", group = "client"}
     ),
+
+    -- TAG KEYS
+    -- Change layouts
+    awful.key({ super, shift }, "]",
+      function ()
+        awful.layout.inc(1)
+      end,
+      {description = "select next layout", group = "tag"}
+    ),
+    awful.key({ super, shift }, "[",
+      function ()
+        awful.layout.inc(1)
+      end,
+      {description = "select prev layout", group = "tag"}
+    ),
+    -- adjust gaps
+    awful.key({ super, shift }, 0,
+      function ()
+        awful.tag.incgap(4)
+      end,
+      {description = "increase gaps", group = "tag"}
+    ),
+    awful.key({ super, shift }, 9,
+      function ()
+        awful.tag.incgap(-4)
+      end,
+      {description = "decrease gaps", group = "tag"}
+    ),
+    -- Go to next tag
+    awful.key({ super }, "Tab",
+      function ()
+        awful.tag.viewnext()
+      end,
+      {description = "next tag", group = "tag"}
+    ),
+    -- Go to previous tag
+    -- TODO: NOT WORKING
+    awful.key({ super, shift }, "Tab",
+      function ()
+        awful.tag.viewnext()
+      end,
+      {description = "next tag", group = "tag"}
+    ),
+
+    -- AWESOME KEYS
     -- show hotkeys Super + ?
-    awful.key({ superkey, "Shift" }, "#61",
+    awful.key({ super, shift }, "/",
       function ()
         hotkeys_popup.show_help(nil, awful.screen.focused())
       end,
       {description = "show main menu", group = "awesome"}
     ),
-    -- Layout manipulation
-    awful.key({ superkey }, ")",
-      function ()
-        awful.layout.inc(1)
-      end,
-      {description = "select next", group = "layout"}
+    -- quit awesome
+    awful.key({ super, ctrl }, "q",
+      awesome.quit,
+      {description = "quit awesome", group = "awesome"}
     ),
-    awful.key({ superkey }, ",",
-      function ()
-        awful.layout.inc(-1)
-      end,
-      {description = "select previous", group = "layout"}
-    ),
-    awful.key({ superkey }, "[",
+    -- restart awesome
+    awful.key({ super, ctrl }, "r",
+      awesome.restart,
+      {description = "reload awesome", group = "awesome"}
+      ),
+
+    -- SCREEN KEYS
+    awful.key({ super }, "]",
       function ()
         awful.screen.focus_relative(1)
       end,
       {description = "focus the next screen", group = "screen"}
     ),
-    awful.key({ superkey }, "]",
+    awful.key({ super }, "[",
       function ()
         awful.screen.focus_relative(-1)
       end,
       {description = "focus the previous screen", group = "screen"}
     ),
-    -- Go to previous client
-    awful.key({ superkey }, "Tab",
-      function ()
-          awful.client.focus.history.previous()
-          if client.focus then
-              client.focus:raise()
-          end
-      end,
-      {description = "go back", group = "client"}
-    ),
-    -- Standard program
-    awful.key({ altkey }, "Return",
+
+    -- Launchers
+    -- Terminal
+    awful.key({ alt }, "Return",
       function ()
         awful.spawn(user.terminal)
       end,
       {description = "open a terminal", group = "launcher"}
     ),
-    -- Standard program
-    awful.key({ altkey }, "#60",
+    -- powermenu
+    -- TODO: Make this a lua widget and bind to bar
+    awful.key({ alt }, "period",
       function ()
         awful.spawn(os.getenv("HOME") .. "/.config/polybar/scripts/powermenu")
       end,
-      {description = "open up sysmenu", group = "launcher"}
+      {description = "sysmenu", group = "launcher"}
     ),
-
-    -- AWESOMEWM binds
-    awful.key({ superkey, "Shift" }, "q",
-      awesome.quit,
-      {description = "quit awesome", group = "awesome"}
+    -- emoji selector
+    awful.key({ alt }, "e", 
+      function ()
+        awful.spawn('rofi -show emoji -modi emoji')
+      end,
+      {description = "emoji picker", group = "launcher"}
     ),
-    awful.key({ superkey, "Control" }, "r",
-      awesome.restart,
-      {description = "reload awesome", group = "awesome"}
-      ),
-
+    awful.key({ alt }, "f",
+      function ()
+        awful.spawn('delta-emoji-picker')
+      end,
+      {description = "delta picker", group = "launcher"}
+    ),
     -- Scratchpads
-    awful.key({ superkey }, "/",
+    awful.key({ alt }, "/",
       function ()
         awesome.emit_signal("scratch::bench")
       end,
-      {description = "launch bench scratchpad", group = "scratchpads"}
+      {description = "show bench", group = "scratchpads"}
     ),
-    awful.key({ superkey, "Shift" }, "t",
+    -- show terminal scratch alt + >
+    awful.key({ alt, shift }, ".",
       function ()
         awesome.emit_signal("scratch::terminal")
       end,
-      {description = "launch terminal scratchpad", group = "scratchpads"}
+      {description = "show terminal", group = "scratchpads"}
     ),
-    awful.key({ superkey }, ";",
+    awful.key({ alt }, ";",
       function ()
         awesome.emit_signal("scratch::monitor")
       end,
-      {description = "launch monitor scratchpad", group = "scratchpads"}
+      {description = "show sys monitor", group = "scratchpads"}
     ),
-    awful.key({ superkey, altkey }, ";",
+    awful.key({ alt }, "q",
       function ()
         awesome.emit_signal("scratch::quest")
       end,
-      {description = "launch quest scratchpad", group = "scratchpads"}
+      {description = "show quest", group = "scratchpads"}
     ),
-    awful.key({ superkey, altkey }, "t",
+    awful.key({ alt }, "t",
       function ()
         awesome.emit_signal("scratch::task")
       end,
@@ -170,12 +208,12 @@ local globalkeys = gears.table.join(
     ),
     -- END MY KEYS
 
-    awful.key({ superkey }, "u",
+    awful.key({ super }, "u",
       awful.client.urgent.jumpto,
       {description = "jump to urgent client", group = "client"}
     ),
 
-    awful.key({ superkey, "Control" }, "n",
+    awful.key({ super, ctrl }, "n",
       function ()
           local c = awful.client.restore()
           -- Focus restored client
@@ -187,31 +225,10 @@ local globalkeys = gears.table.join(
       end,
       {description = "restore minimized", group = "client"}
     ),
-
-    -- Prompt
-    awful.key({ superkey }, "r",
-      function ()
-        awful.screen.focused().mypromptbox:run()
-      end,
-      {description = "run prompt", group = "launcher"}
-    ),
-    awful.key({ superkey }, "x",
-      function ()
-          awful.prompt.run {
-            prompt       = "Run Lua code: ",
-            textbox      = awful.screen.focused().mypromptbox.widget,
-            exe_callback = awful.util.eval,
-            history_path = awful.util.get_cache_dir() .. "/history_eval"
-          }
-      end,
-      {description = "lua execute prompt", group = "awesome"}
-    ),
-    -- Menubar
-    awful.key({ altkey }, "d",
+    awful.key({ alt }, "d",
       function ()
         awful.spawn("rofi -show drun -show-icons -location 0 -yoffset -250")
       end,
-    --   menubar.show,
       {description = "show the menubar", group = "launcher"}
     )
 )
@@ -220,62 +237,76 @@ clientbuttons = gears.table.join(
     awful.button({ }, 1, function (c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
     end),
-    awful.button({ superkey }, 1, function (c)
+    awful.button({ super }, 1, function (c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
         awful.mouse.client.move(c)
     end),
-    awful.button({ superkey }, 3, function (c)
+    awful.button({ super }, 3, function (c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
         awful.mouse.client.resize(c)
     end)
 )
 
 local clientkeys = gears.table.join(
-    awful.key({ superkey }, "f",
+    -- Move focused client to other screen
+    awful.key({ super, alt }, "]",
+      function (c)
+        -- defaults to next screen
+        c:move_to_screen()
+      end,
+      {description = "Move client to other screen", group = "client"}
+    ),
+    awful.key({ super }, "f",
       function (c)
           c.fullscreen = not c.fullscreen
           c:raise()
       end,
       {description = "toggle fullscreen", group = "client"}
     ),
-    awful.key({ superkey }, "w",
+    awful.key({ super }, "z",
+      function (c)
+        awful.client.setmaster(c)
+      end,
+      {description = "set as master", group = "client"}
+    ),
+    awful.key({ super }, "w",
       function (c)
         c:kill()
       end,
       {description = "close", group = "client"}
     ),
-    awful.key({ superkey, "Control" }, "space",  awful.client.floating.toggle                     ,
+    awful.key({ super, ctrl }, "space",  awful.client.floating.toggle                     ,
               {description = "toggle floating", group = "client"}),
-    awful.key({ superkey }, "z",
+    awful.key({ super }, "z",
         function (c)
           c:swap(awful.client.getmaster())
         end,
         {description = "move to master", group = "client"}
     ),
-    awful.key({ superkey,           }, "o",      function (c) c:move_to_screen()               end,
+    awful.key({ super,           }, "o",      function (c) c:move_to_screen()               end,
               {description = "move to screen", group = "client"}),
-    awful.key({ superkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
+    awful.key({ super,           }, "t",      function (c) c.ontop = not c.ontop            end,
               {description = "toggle keep on top", group = "client"}),
-    awful.key({ superkey,           }, "n",
+    awful.key({ super,           }, "n",
         function (c)
             -- The client currently has the input focus, so it cannot be
             -- minimized, since minimized clients can't have the focus.
             c.minimized = true
         end ,
         {description = "minimize", group = "client"}),
-    awful.key({ superkey,           }, "m",
+    awful.key({ super,           }, "m",
         function (c)
             c.maximized = not c.maximized
             c:raise()
         end ,
         {description = "(un)maximize", group = "client"}),
-    awful.key({ superkey, "Control" }, "m",
+    awful.key({ super, ctrl }, "m",
         function (c)
             c.maximized_vertical = not c.maximized_vertical
             c:raise()
         end ,
         {description = "(un)maximize vertically", group = "client"}),
-    awful.key({ superkey, "Shift"   }, "m",
+    awful.key({ super, shift   }, "m",
         function (c)
             c.maximized_horizontal = not c.maximized_horizontal
             c:raise()
@@ -289,7 +320,7 @@ local clientkeys = gears.table.join(
 for i = 1, 9 do
     globalkeys = gears.table.join(globalkeys,
         -- View tag only.
-        awful.key({ superkey }, "#" .. i + 9,
+        awful.key({ super }, "#" .. i + 9,
                   function ()
                         local screen = awful.screen.focused()
                         local tag = screen.tags[i]
@@ -299,7 +330,7 @@ for i = 1, 9 do
                   end,
                   {description = "view tag #"..i, group = "tag"}),
         -- Toggle tag display.
-        awful.key({ superkey, "Control" }, "#" .. i + 9,
+        awful.key({ super, ctrl }, "#" .. i + 9,
                   function ()
                       local screen = awful.screen.focused()
                       local tag = screen.tags[i]
@@ -309,7 +340,7 @@ for i = 1, 9 do
                   end,
                   {description = "toggle tag #" .. i, group = "tag"}),
         -- Move client to tag.
-        awful.key({ superkey, "Shift" }, "#" .. i + 9,
+        awful.key({ super, alt }, "#" .. i + 9,
                   function ()
                       if client.focus then
                           local tag = client.focus.screen.tags[i]
@@ -318,18 +349,8 @@ for i = 1, 9 do
                           end
                      end
                   end,
-                  {description = "move focused client to tag #"..i, group = "tag"}),
-        -- Toggle tag on focused client.
-        awful.key({ superkey, "Control", "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus then
-                          local tag = client.focus.screen.tags[i]
-                          if tag then
-                              client.focus:toggle_tag(tag)
-                          end
-                      end
-                  end,
-                  {description = "toggle focused client on tag #" .. i, group = "tag"})
+                  {description = "move focused client to tag #"..i, group = "tag"}
+        )
     )
 end
 
@@ -337,11 +358,11 @@ local clientbuttons = gears.table.join(
     awful.button({ }, 1, function (c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
     end),
-    awful.button({ superkey }, 1, function (c)
+    awful.button({ super }, 1, function (c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
         awful.mouse.client.move(c)
     end),
-    awful.button({ superkey }, 3, function (c)
+    awful.button({ super }, 3, function (c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
         awful.mouse.client.resize(c)
     end)

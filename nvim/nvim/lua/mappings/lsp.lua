514e80a31be9ede85a6e545d54c7b25a54e8d147
Stephen Bolton <stephen@bitsonthemind.com>
2021-09-10 11:38:14 -0600

    Add vista mapping for file symbols

--- nvim/nvim/lua/mappings/lsp.lua
+++ nvim/nvim/lua/mappings/lsp.lua
@@ -6,7 +6,7 @@ map('n', '<leader>lD', ':vsp <BAR> :lua vim.lsp.buf.definition()<CR>zz', { norem
 map('n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
 map('n', '<leader>lh', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
 map('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting_sync(nil, 1000)<CR>', default_opts)
-map('n', '<leader>lo', '<cmd>lua require("telescope.builtin").lsp_document_symbols()<CR>', default_opts)
+map('n', '<leader>lo', ':Vista finder skim<cr>', default_opts)
 map('n', '<leader>l[', '<cmd>lua vim.lsp.diagnostic.goto_prev({ wrap = true })<CR>', default_opts)
 map('n', '<leader>l]', '<cmd>lua vim.lsp.diagnostic.goto_next({ wrap = true })<CR>', default_opts)
 map('n', '<leader>l=', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({})<CR>', default_opts)

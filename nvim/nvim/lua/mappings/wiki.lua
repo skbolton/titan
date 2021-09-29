9224b696bf79595a515bd7921e05d29aea777db4
Stephen Bolton <stephen@bitsonthemind.com>
2021-09-29 12:04:58 -0600

    Add mappings for jumping to diary index and entries

--- nvim/nvim/lua/mappings/wiki.lua
+++ nvim/nvim/lua/mappings/wiki.lua
@@ -1,2 +1,7 @@
 local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true}
+
+map('n', '<leader><DOWN>', '<cmd>VimwikiMakeDiaryNote<cr>', default_opts)
+map('n', '<leader><UP>', '<cmd>VimwikiDiaryIndex<cr>', default_opts)
+map('n', '<leader><LEFT>', '<cmd>VimwikiDiaryPrevDay<cr>', default_opts)
+map('n', '<leader><RIGHT>', '<cmd>VimwikiDiaryNextDay<cr>', default_opts)

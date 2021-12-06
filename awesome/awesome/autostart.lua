c0ff3e43ac733d86336bd80f2c8243f6de641019
Stephen Bolton <stephen@bitsonthemind.com>
2021-12-05 21:47:54 -0700

    [AWE] Fix launching NNN with proper environment

--- awesome/awesome/autostart.lua
+++ awesome/awesome/autostart.lua
@@ -9,7 +9,7 @@ awful.spawn.once("playerctld daemon")
 awful.spawn.once("nm-applet --no-agent")
 
 -- GUIS
-awful.spawn("kitty --class NNN /home/orlando/.local/bin/nnn")
+awful.spawn("kitty --class NNN zsh -c ~/.local/bin/nnn")
 awful.spawn("kitty --class spotify-tui tmux start ';' attach -t spotify")
 awful.spawn.once("mailspring")
 awful.spawn.once("slack")

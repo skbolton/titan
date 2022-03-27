9c62dda108117d4a35ca810b4995546d71ce3a1e
Stephen Bolton <stephen@bitsonthemind.com>
2022-03-26 19:16:08 -0600

    [NVIM] Update git signs mappings

--- nvim/nvim/lua/git.lua
+++ nvim/nvim/lua/git.lua
@@ -8,17 +8,17 @@ gitsigns.setup {
     topdelete    = {hl = 'GitGutterDelete', text = '│'},
     changedelete = {hl = 'GitGutterDelete', text = '│'},
   },
-  keymaps = {
-    -- Default keymap options
-    noremap = true,
-    buffer = true,
+  on_attach = function(bufnr)
+    local function map(mode, lhs, rhs, opts)
+        opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+        vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+    end
 
-    ['n <leader>g>'] = '<cmd>lua require\"gitsigns\".next_hunk()<CR>zz',
-    ['n <leader>g<'] = '<cmd>lua require\"gitsigns\".prev_hunk()<CR>zz',
-
-    ['n <leader>g+'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ['n <leader>g-'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <leader>g='] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['n <leader>gp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>'
-  },
+    map('n', ']g', '<cmd>lua require\"gitsigns\".next_hunk()<CR>zz')
+    map('n', '[g', '<cmd>lua require\"gitsigns\".prev_hunk()<CR>zz')
+    map('n', '<leader>g+', '<cmd>lua require"gitsigns".stage_hunk()<CR>')
+    map('n', '<leader>g-', '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>')
+    map('n', '<leader>g=', '<cmd>lua require"gitsigns".reset_hunk()<CR>')
+    map('n', '<leader>gp', '<cmd>lua require"gitsigns".preview_hunk()<CR>')
+  end
 }

741bbd44b0c9e8331cfbfc142ddfc59caa41bb91
Stephen Bolton <stephen@bitsonthemind.com>
2022-09-01 12:52:42 -0400

    [NVIM] Move dadbod config to packer callback

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -34,7 +34,14 @@ packer.startup(function()
     requires = {
       'kristijanhusak/vim-dadbod-ui',
       'kristijanhusak/vim-dadbod-completion'
-    }
+    },
+    config = function()
+      vim.g.db_ui_use_nerd_fonts = true
+      vim.g.db_ui_execute_on_save = false
+      vim.g.db_ui_save_location = "./steve_queries"
+      vim.g.db_ui_disable_mappings = true
+      vim.g.db_url = os.getenv("DBUI_URL")
+    end
   }
 
   -- Git
--- nvim/nvim/plugin/sql.vim
+++
@@ -1,5 +0,0 @@
-let g:db_ui_use_nerd_fonts = 1
-let g:db_ui_execute_on_save = 0
-let g:db_ui_save_location = "./steve_queries"
-let g:db_ui_disable_mappings = 1
-let g:db_url = $DBUI_URL

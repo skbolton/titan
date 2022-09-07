bda6d11f79c0ba5d154120a2fb95eca84e91393e
Stephen Bolton <stephen@bitsonthemind.com>
2022-09-06 21:38:43 -0400

    [NVIM] Chore: Remove commented out code

--- nvim/nvim/lua/tree-sitter.lua
+++ nvim/nvim/lua/tree-sitter.lua
@@ -1,15 +1,5 @@
 local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
 
---[[ parser_config.elixir = {
-  install_info = {
-    -- url = "~/projects/tree-sitter-zimbu", -- local path or git repo
-    url = "~/Repos/tree-sitter-elixir", -- local path or git repo
-    files = {"src/parser.c"}
-  },
-  filetype = "ex", -- if filetype does not agrees with parser name
-  used_by = {"eex", "exs"} -- additional filetypes that use this parser
-} ]]
-
 require "nvim-treesitter.configs".setup {
   ensure_installed = {'sql', 'bash', 'beancount', 'elixir', 'norg', 'markdown', 'markdown_inline', 'yaml'},
   highlight = {

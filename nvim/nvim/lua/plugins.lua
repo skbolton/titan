e9034e87f78f87835621550f30e78db8f4f94263
Stephen Bolton <stephen@bitsonthemind.com>
2022-09-06 21:37:10 -0400

    [NVIM] Default colorscheme to embark
    This is also what firenvim will use

--- nvim/nvim/lua/plugins.lua
+++ nvim/nvim/lua/plugins.lua
@@ -165,7 +165,10 @@ packer.startup(function()
 
   use {
     'embark-theme/vim',
-    cond = function() return os.getenv('THEME') == 'embark' end,
+    cond = function()
+      local theme = os.getenv("THEME")
+      return theme == 'embark' or theme == nil
+    end,
     config = function()
       vim.g.embark_terminal_italics = 1
       vim.cmd('colorscheme embark')

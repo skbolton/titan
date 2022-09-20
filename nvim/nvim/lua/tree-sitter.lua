2867398081c871480ca6ddffa7b12d32331a5c56
Stephen Bolton <stephen@bitsonthemind.com>
2022-09-20 19:28:44 -0400

    [NVIM] Add eex and heex tressitter grammars

--- nvim/nvim/lua/tree-sitter.lua
+++ nvim/nvim/lua/tree-sitter.lua
@@ -1,7 +1,7 @@
 local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
 
 require "nvim-treesitter.configs".setup {
-  ensure_installed = {'sql', 'bash', 'beancount', 'elixir', 'norg', 'markdown', 'markdown_inline', 'yaml'},
+  ensure_installed = {'sql', 'bash', 'beancount', 'eex', 'heex', 'elixir', 'norg', 'markdown', 'markdown_inline', 'yaml'},
   highlight = {
     enable = true,
     disable = {'elixir'}

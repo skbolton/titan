a3bc93e4ba58c36a1baa777af38beb1d488af061
Stephen Bolton <stephen@bitsonthemind.com>
2023-02-14 23:09:39 -0500

    [AWE] Switch default layout
    Now that I am running arch so much in a split screen a horizontal layout
    makes more sense as the default.

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -66,11 +66,11 @@ user = {
 -- TODO: Move this to its own module? (tag.lua)
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
+  awful.layout.suit.fair.horizontal,
   bling.layout.centered,
   awful.layout.suit.tile.left,
   awful.layout.suit.tile,
   awful.layout.suit.fair,
-  awful.layout.suit.fair.horizontal,
   -- awful.layout.suit.spiral,
   -- awful.layout.suit.spiral.dwindle,
   awful.layout.suit.max,

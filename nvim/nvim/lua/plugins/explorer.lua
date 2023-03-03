e6fb0d6df1a805e1e231baff4011307a3fea41cd
Stephen Bolton <stephen@bitsonthemind.com>
2023-03-02 22:17:08 -0500

    [NVIM] Change nnn horizontal split keybind

--- nvim/nvim/lua/plugins/explorer.lua
+++ nvim/nvim/lua/plugins/explorer.lua
@@ -5,7 +5,7 @@ return {
     require('nnn').setup {
       mappings = {
         { "<C-t>", nnn.builtin.open_in_tab },       -- open file(s) in tab
-        { "<C-s>", nnn.builtin.open_in_split },     -- open file(s) in split
+        { "<C-x>", nnn.builtin.open_in_split },     -- open file(s) in split
         { "<C-v>", nnn.builtin.open_in_vsplit },    -- open file(s) in vertical split
       }
     }
